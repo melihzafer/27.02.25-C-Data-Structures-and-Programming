4536106e708d6153c4b7807762fd73e8f871481f
Kabinet 50 <126640@student.ue-varna.bg>
2025-02-27 11:31:27 +0200

    Add fruit input, array processing, and string replacement s Removed initial greeting message. Added user prompts for five fruit inputs and stored them in an array. Implemented a loop to print fruits starting with 'A' or 'K'. Introduced replacingString() method to modify and print input strings.

--- 27.02.25/Program.cs
+++ 27.02.25/Program.cs
@@ -6,7 +6,36 @@ namespace MyNampespace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello C# lovers!");
+            Console.WriteLine("Type a fruit1:");
+            string fruit1 = Console.ReadLine();
+            Console.WriteLine("Type a fruit2:");
+            string fruit2 = Console.ReadLine();
+            Console.WriteLine("Type a fruit3:");
+            string fruit3 = Console.ReadLine();
+            Console.WriteLine("Type a fruit4:");
+            string fruit4 = Console.ReadLine();
+            Console.WriteLine("Type a fruit5:");
+            string fruit5 = Console.ReadLine();
+
+            string[] fruits = new string[] { fruit1, fruit2, fruit3, fruit4, fruit5 };
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (fruits[i].StartsWith("A") || fruits[i].StartsWith("K"))
+                {
+                    Console.WriteLine(fruits[i]);
+                }
+            }
+        }
+        public void replacingString()
+        {
+            string s = Console.ReadLine();
+            string zamqna1 = s.Replace("C#", "C++");
+
+            if (zamqna1.StartsWith("Aleksandar"))
+            {
+                zamqna1 = zamqna1.Replace("Aleksandar", "Alex");
+            }
+            Console.WriteLine(zamqna1);
         }
     }
 }
