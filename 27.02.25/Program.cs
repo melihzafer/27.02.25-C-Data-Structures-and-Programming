6149d2d4813b9610ec43aba830b940793bc0515f
Kabinet 50 <126640@student.ue-varna.bg>
2025-02-27 11:42:24 +0200

    Add new project Zadacha3 and update Program.cs methods
    Updated solution file to include new project Zadacha3 with its configuration settings. Modified Program.cs in MyNamespace to comment out old fruit filtering code and added two new static methods: replacingString and outputTheFruitsStartWithA. Added new project file Zadacha3.csproj with SDK, output type, target framework (net8.0), and enabled implicit usings and nullable reference types.

--- 27.02.25/Program.cs
+++ 27.02.25/Program.cs
@@ -6,27 +6,11 @@ namespace MyNampespace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type a fruit1:");
-            string fruit1 = Console.ReadLine();
-            Console.WriteLine("Type a fruit2:");
-            string fruit2 = Console.ReadLine();
-            Console.WriteLine("Type a fruit3:");
-            string fruit3 = Console.ReadLine();
-            Console.WriteLine("Type a fruit4:");
-            string fruit4 = Console.ReadLine();
-            Console.WriteLine("Type a fruit5:");
-            string fruit5 = Console.ReadLine();
+            //replacingString();
+            //outputTheFruitsStartWithA();
 
-            string[] fruits = new string[] { fruit1, fruit2, fruit3, fruit4, fruit5 };
-            for (int i = 0; i < fruits.Length; i++)
-            {
-                if (fruits[i].StartsWith("A") || fruits[i].StartsWith("K"))
-                {
-                    Console.WriteLine(fruits[i]);
-                }
-            }
         }
-        public void replacingString()
+        static void replacingString()
         {
             string s = Console.ReadLine();
             string zamqna1 = s.Replace("C#", "C++");
@@ -36,6 +20,23 @@ namespace MyNampespace
                 zamqna1 = zamqna1.Replace("Aleksandar", "Alex");
             }
             Console.WriteLine(zamqna1);
+        }
+
+
+        static void outputTheFruitsStartWithA()
+        {
+            string input = Console.ReadLine();
+            string[] fruits = input.Split(", ");
+            //Console.WriteLine(fruits);
+            string newSttring = "";
+            foreach (string fruit in fruits)
+            {
+                if (fruit.StartsWith("a") || fruit.StartsWith("k"))
+                {
+                    newSttring += fruit + ", ";
+                }
+            }
+            Console.WriteLine(newSttring);
         }
     }
 }
